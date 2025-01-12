b5b78b8c950e1260afb6eee729b70fce757cbc76
Ceedrich <cedric@lehr.ch>
2025-01-12 19:31:00 +0100

    update gui message(s)

--- src/main/java/beaconextender/mixin/BeaconScreenMixin.java
+++ src/main/java/beaconextender/mixin/BeaconScreenMixin.java
@@ -3,25 +3,22 @@ package beaconextender.mixin;
 import beaconextender.BeaconExtender;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.BeaconScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.BeaconMenu;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.reflect.Field;
-
 @Mixin(BeaconScreen.class)
 public class BeaconScreenMixin {
     @Unique
-    private static final Component LAYERS_LABEL = Component.translatable("block.minecraft.beacon.bannerextender.layers");
+    private static final Component ACTIVE_LAYERS_LABEL = Component.translatable("block.minecraft.beacon.beaconextender.active_layers");
+
+    @Unique
+    private static final Component INACTIVE_LABEL = Component.translatable("block.minecraft.beacon.beaconextender.inactive");
 
     @Inject(at = @At("HEAD"), method = "renderLabels")
     private void renderLabels(GuiGraphics guiGraphics, int i, int j, CallbackInfo ci) {
@@ -32,7 +29,11 @@ public class BeaconScreenMixin {
             BeaconMenu menu = instance.getMenu();
             int layers = menu.getLevels();
 
-            guiGraphics.drawCenteredString(font, LAYERS_LABEL.getString() + layers, 169, 80, 14737632);
+            if (layers == 0) {
+                guiGraphics.drawCenteredString(font, INACTIVE_LABEL.getString(), 169, 80, 14737632);
+            } else {
+                guiGraphics.drawCenteredString(font, ACTIVE_LAYERS_LABEL.getString() + layers, 169, 80, 14737632);
+            }
         }
     }
 }
